bb002668445714793de7687ae7df4c942eb398b3
Ami0480 <amifuku80@gmail.com>
2025-10-24 00:36:20 +0800

    Created navbar page

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import Header from "./Header";
 
 import "./App.css";
 
@@ -6,27 +7,33 @@ import BackgroundVideo from "./images/background-video.mp4";
 
 function App() {
   return (
-    <div className="relative w-full">
-      <video
-        src={BackgroundVideo}
-        autoPlay
-        loop
-        muted
-        playsInline
-        className="absolute w-full h-screen object-cover "
-      />
-      <div className="absolute w-full h-screen bg-gradient-to-b from-black via-transparent to-transparent"></div>
-      <div className="absolute w-full h-screen flex flex-col items-center justify-center">
-        <h1 className=" text-white text-6xl font-[--font-playfair] italic font-bold">
-          Nature Spa
-        </h1>
-        <Link
-          to="/booking"
-          className="pr-5 pl-5 pt-3 pb-3 mt-5 text-white rounded-full"
-          style={{ backgroundColor: "#af9b8e" }}
-        >
-          Book Now
-        </Link>
+    <div className="App">
+      <div className="relative w-full">
+        <video
+          src={BackgroundVideo}
+          autoPlay
+          loop
+          muted
+          playsInline
+          className="absolute w-full h-screen object-cover "
+        />
+        <div className="absolute w-full h-screen bg-gradient-to-b from-black via-transparent to-transparent"></div>
+        <div className="absolute w-full z-40">
+          <Header />
+        </div>
+
+        <div className="absolute w-full h-screen flex flex-col items-center justify-center">
+          <h1 className=" text-white text-6xl font-[playfair] italic font-bold">
+            Nature Spa
+          </h1>
+          <Link
+            to="/booking"
+            className="pr-5 pl-5 pt-3 pb-3 mt-5 text-white rounded-full font-[lato] font-bold"
+            style={{ backgroundColor: "#af9b8e" }}
+          >
+            Book Now
+          </Link>
+        </div>
       </div>
     </div>
   );
---
+++ my-app/src/Header.jsx
@@ -0,0 +1,78 @@
+import React, { useState, useEffect } from "react";
+import { Link } from "react-router-dom";
+
+import logo from "./images/Logo.svg";
+import hamburger from "./images/hamburger-icon.svg";
+import cross from "./images/cross.png";
+
+export default function Header() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, []);
+
+  return (
+    <div className="header">
+      <div className="container flex place-content-between">
+        <Link to="/">
+          <img src={logo} />
+        </Link>
+        <img src={hamburger} onClick={() => setMenuOpen(true)} />
+      </div>
+      {menuOpen && (
+        <div
+          className="fixed inset-0  z-20"
+          onClick={() => setMenuOpen(false)}
+        ></div>
+      )}
+
+      <section
+        className={`navbar fixed top-0 flex flex-col items-center justify-center gap-y-9
+
+          bg-[#917a6b]
+        w-full
+        h-screen
+        transform
+        transition-transform
+        duration-500
+        ease-in-out
+        z-30 ${menuOpen ? "translate-x-0" : "translate-x-full"} 
+        `}
+      >
+        <img
+          src={cross}
+          onClick={() => setMenuOpen(false)}
+          className="absolute top-4 right-4 w-10 h-10"
+        />
+        <Link
+          to="/"
+          onClick={() => setMenuOpen(false)}
+          className="hover:underline decoration-1 duration-200"
+        >
+          Home
+        </Link>
+        <Link
+          to="/about"
+          onClick={() => setMenuOpen(false)}
+          className="hover:underline decoration-1 duration-200"
+        >
+          About
+        </Link>
+        <Link
+          to="/menu"
+          onClick={() => setMenuOpen(false)}
+          className="hover:underline decoration-1 duration-200"
+        >
+          Menu
+        </Link>
+        <Link
+          to="/contact"
+          onClick={() => setMenuOpen(false)}
+          className="hover:underline decoration-1 duration-200"
+        >
+          Contact
+        </Link>
+      </section>
+    </div>
+  );
+}
