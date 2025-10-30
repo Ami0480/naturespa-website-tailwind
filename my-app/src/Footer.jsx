883bd3971335ca6c20610be98f3fccb67d73f6d3
Ami0480 <amifuku80@gmail.com>
2025-10-30 14:13:18 +0800

    Added copywrite

--- my-app/src/Footer.jsx
+++ my-app/src/Footer.jsx
@@ -16,7 +16,7 @@ export default function Footer() {
 
       <div className="flex">
         <div className="flex flex-1">
-          <ul className="font-raleway mt-5 flex flex-col gap-3">
+          <ul className="font-raleway mt-5 flex flex-col gap-3 text-[20px]">
             <li>Home</li>
             <li>About</li>
             <li>Menu</li>
@@ -25,13 +25,13 @@ export default function Footer() {
         </div>
 
         <div className="flex flex-1">
-          <ul className="font-raleway flex mt-5 flex-col gap-3">
+          <ul className="font-raleway flex mt-5 flex-col gap-3 text-[20px]">
             <li>08-100-200</li>
             <li>10 Perth St, Perth</li>
             <li>
               <a href="mailto:naturespa@gmail.com">naturespa@gmail.com</a>
             </li>
-            <li className="flex gap-4">
+            <li className="flex gap-5">
               <a
                 href="https://www.facebook.com"
                 target="_blank"
@@ -54,22 +54,23 @@ export default function Footer() {
         </div>
       </div>
 
-      <div className="relative mt-10 flex flex-col gap-3 font-raleway">
+      <div className="relative mt-10 flex flex-col gap-3 font-raleway text-[20px]">
         <p>Subscribe</p>
         <form>
           <input
             type="email"
             placeholder="Please enter your email..."
-            className="w-full h-9 bg-white opacity-40 text-[#9c8f86] px-2"
+            className="w-full h-9 bg-white opacity-40 text-[#9c8f86] px-2 text-[16px]"
           />
           <button
             type="submit"
-            className="absolute w-5 h-5 top-11 right-0 text-[#9c8f86]"
+            className="absolute w-5 h-5 top-12.5 right-0 text-[#9c8f86]"
           >
             <img src={SubmitArrow} />
           </button>
         </form>
       </div>
+      <p className="text-[14px] mt-10">NATURE@2025</p>
     </div>
   );
 }
